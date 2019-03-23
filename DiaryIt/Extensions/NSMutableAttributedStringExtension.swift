3abb0a064c25c54228a050f8514e2e18d20ece74
Rinni Swift <rinradaswift@gmail.com>
2019-03-23 14:17:45 -0700

    added extension on NSMutableString to set font to Avenir-Book 17

--- DiaryIt/Extensions/NSMutableAttributedStringExtension.swift
+++ DiaryIt/Extensions/NSMutableAttributedStringExtension.swift
@@ -9,4 +9,25 @@
 import Foundation
 import UIKit
 
-extension NSMUt
+extension NSMutableAttributedString {
+    
+    func setFont() {
+        let font = UIFont(name: "Avenir-Book", size: 17)
+        let color = UIColor(red:0.42, green:0.42, blue:0.42, alpha:1.0)
+        
+        beginEditing()
+        self.enumerateAttribute(.font, in: NSRange(location: 0, length: self.length)) { value, range, stop in
+            if let f = value as? UIFont {
+                let newFontDescriptor = f.fontDescriptor.withFamily((font?.familyName)!).withSymbolicTraits(f.fontDescriptor.symbolicTraits)
+                let newFont = UIFont(descriptor: newFontDescriptor!, size: (font?.pointSize)!)
+                removeAttribute(.font, range: range)
+                addAttribute(.font, value: newFont, range: range)
+                
+                removeAttribute(.foregroundColor, range: range)
+                addAttribute(.foregroundColor, value: color, range: range)
+            }
+        }
+        endEditing()
+    }
+    
+}
