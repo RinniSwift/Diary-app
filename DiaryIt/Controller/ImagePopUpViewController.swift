a75d094437b1a9096b250b1ce86952bc9920fba8
Rinni Swift <rinradaswift@gmail.com>
2019-03-24 23:12:37 -0700

    added gesture recognizer to detect swipe gestures between photos and changed photos accordingly. but, hard coded the index number of images and code never falls in direction .left

--- DiaryIt/Controller/ImagePopUpViewController.swift
+++ DiaryIt/Controller/ImagePopUpViewController.swift
@@ -8,20 +8,70 @@
 
 import UIKit
 
-class ImagePopUpViewController: UIViewController {
+class ImagePopUpViewController: UIViewController, UIScrollViewDelegate {
     // TODO: when tapped on view, dismiss the view controller
     
+    // MARK: - Variables
     var imageToFill: UIImage? = nil
+    var imagesInNote: [NSData] = []
+    var imageIndex: Int = 0
     
+    // MARK: - Outlets
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         imageView.image = imageToFill
+        
+        var swipeGestRecognizer = UISwipeGestureRecognizer(target: self, action: #selector (swiped(gesture:)))
+        self.view.addGestureRecognizer(swipeGestRecognizer)
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.dismiss(animated: false, completion: nil)
+    @objc func swiped(gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            // TODO: switch case never falls in left direction
+            switch swipeGesture.direction {
+                
+            case .right:
+                print("User swiped right")
+                
+                // decrease index first
+                
+                imageIndex -= 1
+                
+                // check if index is in range
+                
+                if imageIndex < 0 {
+                    
+                    imageIndex = 1
+                    
+                }
+                imageView.image = UIImage(data: imagesInNote[imageIndex] as Data)
+            case UISwipeGestureRecognizerDirection.left:
+                print("User swiped Left")
+                
+                // increase index first
+                
+                imageIndex += 1
+                
+                // check if index is in range
+                
+                if imageIndex > 1 {
+                    
+                    imageIndex = 0
+                    
+                }
+            imageView.image = UIImage(data: imagesInNote[imageIndex] as Data)
+            default:
+                break //stops the code/codes nothing.
+            }
+            
+        }
+        
+        
     }
     
+
 }
