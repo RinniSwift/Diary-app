ed1af31751f752d2cf15d90101036efefebf1e76
Rinni Swift <rinradaswift@gmail.com>
2019-03-25 23:37:18 -0700

    added containerViewController to handle how the view will be toggled side to side. ViewController and AlertViewController were embebded into ContainerViewController

--- DiaryIt/Controller/ContainerVC.swift
+++ DiaryIt/Controller/ContainerVC.swift
@@ -8,21 +8,67 @@
 
 import UIKit
 
-class ContainerVC: UIViewController {
+class ContainerViewC: UIViewController {
+    
+    var alertViewController: UIViewController!
+    var centerViewController: UINavigationController!
+    var isExpanded: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureCalendarViewController()
-
+        configureStartViewController()
+        
+        
     }
     
-    func configureCalendarViewController() {
-        let calendarController = ViewController()
-        let controller = UINavigationController(rootViewController: calendarController)
+    func configureStartViewController() {
+        let homeVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "viewControllerID") as! ViewController
+        homeVC.delegate = self
+        centerViewController = UINavigationController(rootViewController: homeVC)
+        centerViewController.setNavigationBarHidden(true, animated: false)
         
-        view.addSubview(controller.view)
-        addChildViewController(controller)
-        controller.didMove(toParentViewController: self)
+        view.addSubview(centerViewController.view)
+        addChildViewController(centerViewController)
+        centerViewController.didMove(toParentViewController: self)
     }
+    
+    func configureRedViewController() {
+        if alertViewController == nil {
+            alertViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "alertViewID") as! AlertViewController
+            view.insertSubview(alertViewController.view, at: 0)
+            addChildViewController(alertViewController)
+            alertViewController.didMove(toParentViewController: self)
+        }
+    }
+    
+    func showRedViewController(shouldExpand: Bool) {
+        
+        if shouldExpand {
+            // show menu
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+                self.centerViewController.view.frame.origin.x = self.centerViewController.view.frame.width - 80
+                }, completion: nil)
+        } else {
+            // hide menu
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+                self.centerViewController.view.frame.origin.x = 0
+            }, completion: nil)
+        }
+        
+    }
+}
+
+
+
+extension ContainerViewC: CalendarControllerDelegate {
+    
+    func handleSideToggle() {
+        if !isExpanded {
+            configureRedViewController()
+        }
+        isExpanded = !isExpanded
+        showRedViewController(shouldExpand: isExpanded)
+    }
+    
 }
