c662af683ffc9b7c72f969fd9936f28af31b417d
Rinni Swift <rinradaswift@gmail.com>
2019-03-25 18:24:33 -0700

    function touchesBegan() checks location of touch if greater than the image, which dimiss gets called

--- DiaryIt/Controller/ImagePopUpViewController.swift
+++ DiaryIt/Controller/ImagePopUpViewController.swift
@@ -13,8 +13,6 @@ class ImagePopUpViewController: UIViewController, UIScrollViewDelegate {
     
     // MARK: - Variables
     var imageToFill: UIImage? = nil
-    var imagesInNote: [NSData] = []
-    var imageIndex: Int = 0
     
     // MARK: - Outlets
     @IBOutlet weak var imageView: UIImageView!
@@ -24,54 +22,13 @@ class ImagePopUpViewController: UIViewController, UIScrollViewDelegate {
         
         imageView.image = imageToFill
         
-        var swipeGestRecognizer = UISwipeGestureRecognizer(target: self, action: #selector (swiped(gesture:)))
-        self.view.addGestureRecognizer(swipeGestRecognizer)
     }
     
-    @objc func swiped(gesture: UIGestureRecognizer) {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first?.location(in: view)
         
-        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
-            // TODO: switch case never falls in left direction
-            switch swipeGesture.direction {
-                
-            case .right:
-                print("User swiped right")
-                
-                // decrease index first
-                
-                imageIndex -= 1
-                
-                // check if index is in range
-                
-                if imageIndex < 0 {
-                    
-                    imageIndex = 1
-                    
-                }
-                imageView.image = UIImage(data: imagesInNote[imageIndex] as Data)
-            case UISwipeGestureRecognizerDirection.left:
-                print("User swiped Left")
-                
-                // increase index first
-                
-                imageIndex += 1
-                
-                // check if index is in range
-                
-                if imageIndex > 1 {
-                    
-                    imageIndex = 0
-                    
-                }
-            imageView.image = UIImage(data: imagesInNote[imageIndex] as Data)
-            default:
-                break //stops the code/codes nothing.
-            }
-            
+        if (touch?.y)! > (view.frame.height - 60) || (touch?.y)! < CGFloat(15)/*doesnt work*/{
+            self.dismiss(animated: true, completion: nil)
         }
-        
-        
     }
-    
-
 }
---
+++ DiaryIt/Helper/NotificationCenterHelper.swift
@@ -0,0 +1,9 @@
+//
+//  NotificationCenterHelper.swift
+//  DiaryIt
+//
+//  Created by Rinni Swift on 3/25/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import Foundation
