9af6f319a1ef3594e689e5c680bffe8b64f94bbb
Rinni Swift <rinradaswift@gmail.com>
2019-03-25 19:18:33 -0700

    added helper function to create and post notifications to the notification center

--- DiaryIt/Helper/NotificationCenterHelper.swift
+++ DiaryIt/Helper/NotificationCenterHelper.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import UserNotifications
+
+class NotificationCenterHelper {
+    
+    class func addNotification() {
+        let center = UNUserNotificationCenter.current()
+        
+        var content = UNMutableNotificationContent()
+        content.title = "Prepare for technical interviews"
+        content.subtitle = "Reminding you to do this!"
+        content.body = "Prepare for technical interviews after eating apples"
+        content.sound = UNNotificationSound.default()
+        
+        let date = Date(timeIntervalSinceNow: 3)
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "content", content: content, trigger: trigger)
+        
+        center.add(request) { err in
+            if err != nil {
+                print(err?.localizedDescription)
+            }
+        }
+    }
+    
+}
