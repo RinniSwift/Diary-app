77091f491e2a95987c6e53b609dfe526446979f3
Rinni Swift <rinradaswift@gmail.com>
2019-03-30 11:14:57 -0700

    added notificationCenter name extension for 'didExpand' when user expands the alertViewController

---
+++ DiaryIt/Extensions/NotificationExtension.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationExtension.swift
+//  DiaryIt
+//
+//  Created by Rinni Swift on 3/30/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let didExpand = Notification.Name("didExpand")
+}
