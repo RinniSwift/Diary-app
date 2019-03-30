9aa8aeaf78b7ac2b40eef796640b03121d6ea133
Rinni Swift <rinradaswift@gmail.com>
2019-03-30 12:49:53 -0700

    added table view cell class and added labels and image for each reminder cell with title and date

--- DiaryIt/View/AlertTableViewCell.swift
+++ DiaryIt/View/AlertTableViewCell.swift
@@ -10,15 +10,7 @@ import UIKit
 
 class AlertTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    @IBOutlet weak var alertTitleLabel: UILabel!
+    @IBOutlet weak var alertDateLabel: UILabel!
+    
 }
