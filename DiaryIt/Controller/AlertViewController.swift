da083b46879ea56acf4a14d3d095f6bce9fb8afb
Rinni Swift <rinradaswift@gmail.com>
2019-03-30 11:18:03 -0700

    added notification listeners to reload table view when alertViewController is expanded

--- DiaryIt/Controller/AlertViewController.swift
+++ DiaryIt/Controller/AlertViewController.swift
@@ -17,12 +17,41 @@ class AlertViewController: UIViewController {
     // MARK: - Actions
     @IBAction func addReminder(_ sender: UIButton) {
         NotificationCenterHelper.addNotification()
+        
+        // TEST:
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5, execute: {
+            let this = CoreDataHelper.retrieveNote().filter{ $0.date == "30 March"}.first
+            print(this?.notifications)
+            for notif in (this?.notifications)! {
+                let notif = notif as! NotificationEntity
+                print(notif.title)
+            }
+            
+            print("reminders: \(NotificationCenterHelper.reminders.count)")
+        })
+        
     }
     
     // MARK: - Variables
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        expandingListeners()
+        // TODO: populate the table view with all retrieved notifications
+    }
+    
+    //TODO: create a notification listener when the alertViewController is expanded.
+    func expandingListeners() {
+        NotificationCenter.default.addObserver(self, selector: #selector(didExpand(_:)), name: .didExpand, object: nil)
+    }
+    
+    @objc func didExpand(_ notification: Notification) {
+        print("notification POSTED: expanding")
+        // TODO: reload table view controller
+    }
+    
+    func getNotifications() {
+        let allNot = CoreDataHelper.retrieveNotifications()
+        print(allNot.count)
     }
 }
