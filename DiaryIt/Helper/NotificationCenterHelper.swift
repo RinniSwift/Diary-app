b2605dcaecf6ec94149d591cff97b29909f44e0a
Rinni Swift <rinradaswift@gmail.com>
2019-03-30 21:59:38 -0700

    modified code for just adding the reminder to the ReminderEntity and not the note entity

--- DiaryIt/Helper/NotificationCenterHelper.swift
+++ DiaryIt/Helper/NotificationCenterHelper.swift
@@ -12,6 +12,7 @@ import UserNotifications
 class NotificationCenterHelper {
     
     class func addNotification() -> NotificationEntity {
+        
         let center = UNUserNotificationCenter.current()
         
         let content = UNMutableNotificationContent()
@@ -24,7 +25,6 @@ class NotificationCenterHelper {
         let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
         
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
-//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
         let request = UNNotificationRequest(identifier: "content", content: content, trigger: trigger)
         
         center.add(request) { err in
@@ -32,57 +32,20 @@ class NotificationCenterHelper {
                 print(err?.localizedDescription)
             }
         }
-        return handleCoreData(notification: content, date: date)
         
+        return saveNotificationToCoreData(notification: content, date: date)
     }
     
-    class func removeNotification(date: String) -> [NotificationEntity] {
-        var identifiers: [String] = []
-        
-        let allNotifications = CoreDataHelper.retrieveNotifications()
-        var notificationAtDate = allNotifications.filter{ $0.date == date }
-        
-        for item in notificationAtDate {
-            identifiers.append("content")
-            CoreDataHelper.deleteNotification(notification: item)
-        }
-
-        let center = UNUserNotificationCenter.current()
-        center.removeDeliveredNotifications(withIdentifiers: identifiers)
-
-        return allNotifications
-    }
-    
-    
-    class func handleCoreData(notification: UNMutableNotificationContent, date: Date) -> NotificationEntity {
+    class func saveNotificationToCoreData(notification: UNMutableNotificationContent, date: Date) -> NotificationEntity {
         
         // create new notification to prepare for adding to array or storing as new array
-        let notif = CoreDataHelper.newNotification(date: date.toString())
+        let notif = CoreDataHelper.newNotification()
         notif.title = notification.title
         notif.subtitle = notification.subtitle
         notif.body = notification.body
         notif.date = date.toString()
         
-        
-        let firstNote = CoreDataHelper.retrieveNote().filter{$0.date == notif.date}.first
-        
-        // check if there are any notes saved at the date of when we will call the notification
-        if firstNote == nil { // note saved at notification date is nil
-            let note = CoreDataHelper.newNote()
-            note.content = nil
-            note.date = date.toString()
-            note.notifications = NSSet(array: [notif])
-        } else { // there're notes saved at the notification date
-            if firstNote?.notifications == nil {
-                firstNote?.notifications = NSSet(array: [notif])
-            } else {
-                var storedNotifs = Array((firstNote?.notifications)!)
-                storedNotifs.append(notif)
-                firstNote?.notifications = NSSet(array: storedNotifs)
-            }
-        }
         CoreDataHelper.saveNote()
         return notif
     }
 }
-
