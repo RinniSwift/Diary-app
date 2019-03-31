16bb0400af484b16bdc9ca58dc5223c65a1ec903
Rinni Swift <rinradaswift@gmail.com>
2019-03-30 18:55:12 -0700

    called the delete notification in the 'willShowNotification' to delete it out of the tableview and coredata

--- DiaryIt/AppDelegate.swift
+++ DiaryIt/AppDelegate.swift
@@ -38,6 +38,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.alert, .badge, .sound])
+        
+        let result = NotificationCenterHelper.removeNotification(date: Date().toString())
+        NotificationCenter.default.post(name: .didDeleteNotif, object: result)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
