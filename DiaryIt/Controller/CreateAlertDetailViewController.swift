072669cc9fe8517824b2f52769254698aba53de6
Rinni Swift <rinradaswift@gmail.com>
2019-03-31 10:21:13 -0700

    added starter code with save and cancle actions to dismiss back to view controller

--- DiaryIt/Controller/CreateAlertDetailViewController.swift
+++ DiaryIt/Controller/CreateAlertDetailViewController.swift
@@ -10,21 +10,19 @@ import UIKit
 
 class CreateAlertDetailViewController: UIViewController {
 
+    // MARK: - Actions
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
