745094d9f34136bd68f9159c4b2cc8032e33e81e
Rinni Swift <rinradaswift@gmail.com>
2019-03-31 10:19:18 -0700

    added 'createAlertViewController'

---
+++ DiaryIt/Controller/CreateAlertDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateAlertDetailViewController.swift
+//  DiaryIt
+//
+//  Created by Rinni Swift on 3/31/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import UIKit
+
+class CreateAlertDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
