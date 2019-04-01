e890944752b9e72633d1a008317bc07c7cfa2973
Rinni Swift <rinradaswift@gmail.com>
2019-04-01 13:48:09 -0700

    time label adjusts accordingly to the date user picks

--- DiaryIt/Controller/CreateAlertDetailViewController.swift
+++ DiaryIt/Controller/CreateAlertDetailViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class CreateAlertDetailViewController: UIViewController {
+    // TODO: create two date pickers for -date and -time
+    // TODO: Maybe when users click on the date/time label, it changes the date picker.
+    
 
     // MARK: - Actions
     @IBAction func saveButtonTapped(_ sender: UIButton) {
@@ -19,10 +22,18 @@ class CreateAlertDetailViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        timeLabel.text = datePicker.date.toHourMinute()
+    }
+    
+    // MARK: - Outlets
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        timeLabel.text = datePicker.date.toHourMinute()
     }
 
 }
