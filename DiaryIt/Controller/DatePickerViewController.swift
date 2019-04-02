e92b0e3bb21ba2486ac059ff77a9551874673970
Rinni Swift <rinradaswift@gmail.com>
2019-04-01 22:57:19 -0700

    new view controller

---
+++ DiaryIt/Controller/DatePickerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DatePickerViewController.swift
+//  DiaryIt
+//
+//  Created by Rinni Swift on 4/1/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
