0c7b159cb8cd220988ba82e26b19c0b8f0c0c019
Rinni Swift <rinradaswift@gmail.com>
2019-04-02 19:59:40 -0700

    button actions segue to date picker or time picker view controllers

--- DiaryIt/Controller/CreateAlertDetailViewController.swift
+++ DiaryIt/Controller/CreateAlertDetailViewController.swift
@@ -9,10 +9,9 @@
 import UIKit
 
 class CreateAlertDetailViewController: UIViewController {
-    // TODO: create two date pickers for -date and -time
     // TODO: Maybe when users click on the date/time label, it changes the date picker.
     
-
+    
     // MARK: - Actions
     @IBAction func saveButtonTapped(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
@@ -22,18 +21,37 @@ class CreateAlertDetailViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
-        timeLabel.text = datePicker.date.toHourMinute()
+    @IBAction func unwindToCreateAlertViewController(_ sender: UIStoryboardSegue) {
+        if sender.source is TimePickerViewController {
+            if let sourceView = sender.source as? TimePickerViewController {
+                timeButton.setTitle(sourceView.dateSelected, for: .normal)
+            }
+        } else if sender.source is DatePickerViewController {
+            if let sourceView = sender.source as? DatePickerViewController {
+                dateButton.setTitle(sourceView.dateSelected, for: .normal)
+            }
+        }
+    }
+    
+    @IBAction func timeButtonTapped(_ sender: UIButton) {
+        // TODO: Instantiate the timePickerViewController and set the date the one in the timeButton
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "timePickerViewController") as! TimePickerViewController
+        self.present(controller, animated: true, completion: nil)
+    }
+    @IBAction func dateButton(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "datePickerViewController") as! DatePickerViewController
+        self.present(controller, animated: true, completion: nil)
     }
     
     // MARK: - Outlets
-    @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timeButton: UIButton!
+    @IBOutlet weak var dateButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        timeLabel.text = datePicker.date.toHourMinute()
+        
     }
 
 }
