8ef05afa69fa38a59c247ff42e6b532b288a9bd5
Rinni Swift <rinradaswift@gmail.com>
2019-04-04 22:23:10 -0700

    added function to set the date picker date to initialy be the date sent from the createAlertViewController

--- DiaryIt/Controller/DatePickerViewController.swift
+++ DiaryIt/Controller/DatePickerViewController.swift
@@ -9,13 +9,14 @@
 import UIKit
 
 class DatePickerViewController: UIViewController {
-    
+
     // MARK: - Variables
     var dateSelected: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setDatePickerDate(dateLabelString: dateSelected)
     }
     
     // MARK: - Outlets
@@ -30,4 +31,13 @@ class DatePickerViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    func setDatePickerDate(dateLabelString: String) {
+        // TODO: create date formatter and set the date picker to equal the date(from: String) format
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd, MMM yyyy"
+        let date = dateFormatter.date(from: dateLabelString + " 2019")
+        print(dateSelected)
+        print(date)
+        datePicker.date = date!
+    }
 }
