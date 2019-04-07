d057c822dc2bcd898bb34bdfdcb0c5ff64539355
Rinni Swift <rinradaswift@gmail.com>
2019-04-06 18:01:01 -0700

    added NotificationCenter to 'post' dateComponents of selected time date

--- DiaryIt/Controller/TimePickerViewController.swift
+++ DiaryIt/Controller/TimePickerViewController.swift
@@ -11,19 +11,20 @@ import UIKit
 class TimePickerViewController: UIViewController {
     
     // MARK: - Variables
-    var dateSelected: String = ""
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        setDatePickerDate(dateLabelString: dateSelected)
-    }
+    var dateSelected: String = ""   // maybe not useful to store it as the string since we already send over the time as dateComponents
+    var savedDateComponent: DateComponents? = nil
     
     // MARK: - Outlets
     @IBOutlet weak var datePicker: UIDatePicker!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setDatePickerDate(dateLabelString: dateSelected)
+    }
     
-    // MARK: - Actions
     @IBAction func saveButtonTapped(_ sender: UIButton) {
+        let components = Calendar.current.dateComponents([.hour, .minute], from: datePicker.date)
+        NotificationCenter.default.post(name: .didAddTime, object: components)
     }
     
     @IBAction func cancelButtonTapped(_ sender: UIButton) {
@@ -35,6 +36,8 @@ class TimePickerViewController: UIViewController {
     }
     
     func setDatePickerDate(dateLabelString: String) {
+        // setting string from datePicker to store in dateSelected var. to send to CreateAlertViewController
+        
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "HH:mm"
         let droppedString = String(dateLabelString.dropLast(3))
