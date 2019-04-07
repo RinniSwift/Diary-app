bd795f11c83b09f6237d59566c3d58c0b6dbc88f
Rinni Swift <rinradaswift@gmail.com>
2019-04-06 20:45:22 -0700

    added

--- DiaryIt/Controller/DatePickerViewController.swift
+++ DiaryIt/Controller/DatePickerViewController.swift
@@ -13,35 +13,39 @@ class DatePickerViewController: UIViewController {
     // MARK: - Variables
     var dateSelected: String = ""
 
-    // MARK: - Outlets
-    @IBOutlet weak var datePicker: UIDatePicker!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setDatePickerDate(dateLabelString: dateSelected)
     }
     
+    // MARK: - Outlets
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     // MARK: - Actions
     @IBAction func dateValueChanged(_ sender: UIDatePicker) {
         dateSelected = datePicker.date.toString()
     }
     
+<<<<<<< HEAD
     @IBAction func saveButtonTapped(_ sender: UIButton) {
         let components = Calendar.current.dateComponents([.year, .month, .day], from: datePicker.date)
         NotificationCenter.default.post(name: .didAddDate, object: components)
     }
     
+=======
+>>>>>>> parent of 69eeb63... added NotificationCenter with 'post' method to send dateComponents of selected date to CreateAlertViewController
     @IBAction func cancelButtonTapped(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
     
     func setDatePickerDate(dateLabelString: String) {
-        // setting string from datePicker to store in dateSelected var. to send to CreateAlertViewController
-
+        // TODO: create date formatter and set the date picker to equal the date(from: String) format
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd MMM yyyy"
+        dateFormatter.dateFormat = "dd, MMM yyyy"
         let date = dateFormatter.date(from: dateLabelString + " 2019")
+        print(dateSelected)
+        print(date)
         datePicker.date = date!
     }
 }
