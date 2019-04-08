003fd5a4469c4fd764dc42a193b7b67c9768f0f2
Rinni Swift <rinradaswift@gmail.com>
2019-04-07 17:48:00 -0700

    added notification listener for when a reminder object is created to reload the alertViewController table view

--- DiaryIt/Controller/AlertViewController.swift
+++ DiaryIt/Controller/AlertViewController.swift
@@ -42,6 +42,7 @@ class AlertViewController: UIViewController {
     func expandingListeners() {
         NotificationCenter.default.addObserver(self, selector: #selector(didExpand(_:)), name: .didExpand, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(didDeleteNotif(_:)), name: .didDeleteNotif, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(didAddReminderObject(_:)), name: .didAddReminderObject, object: nil)
     }
     
     @objc func didExpand(_ notification: Notification) {
@@ -56,7 +57,12 @@ class AlertViewController: UIViewController {
         for item in CoreDataHelper.retrieveNotifications().filter({$0.date == timeOfNotification}) {
             CoreDataHelper.deleteNotification(notification: item)
         }
-        
+    }
+    
+    @objc func didAddReminderObject(_ notification: Notification) {
+        let reminderObj = notification.object as! NotificationEntity
+        allReminders.append(reminderObj)
+        tableView.reloadData()
     }
     
     func getAllNotifications() -> [NotificationEntity] {
